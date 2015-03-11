78448db671392465c116bac1c186d7fa05c14cc7
Shai Alon <shaialon84@gmail.com>
2015-03-11 05:59:23 +0200

    Default to twitter

--- app/router.js
+++ app/router.js
@@ -6,9 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('index' , {path:'/'});
+  this.route('index');
   this.route('widgets');
-  this.route('twitter');
+  this.route('twitter' , {path:'/'});
   this.route('general-ui');
 });
 
